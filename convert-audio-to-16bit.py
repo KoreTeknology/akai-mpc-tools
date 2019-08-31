a03411e5ed2b1907ac849fd1f8b326858a9f03c2
Gábor <gabor.gyorgy@penceo.com>
2019-09-01 00:34:36 +0200

    convert file if samle rate is above 44100

--- convert-audio-to-16bit.py
+++ convert-audio-to-16bit.py
@@ -7,18 +7,31 @@ from folder import folder
 
 file_list = [f for f in iglob(folder, recursive=True) if os.path.isfile(f)]
 error_list = []
+subtype = ''
+is_write = False
 
 with Bar('Processing', max=len(file_list)) as bar:
     for f in file_list:
+        is_write = False
         if f.lower().endswith('wav'):
             try:
                 sound_file = sf.SoundFile(f)
+                subtype = sound_file.subtype
             except Exception as err:
                 error_list.append(err)
             else:
-                if sound_file.subtype != 'PCM_16':
-                    data, samplerate = sf.read(sound_file.name)
-                    # sf.write(sound_file.name, data, samplerate, 'PCM_16')
+                data, samplerate = sf.read(sound_file.name)
+
+                if sound_file.subtype != 'PCM_16' and sound_file.subtype != 'PCM_U8':
+                    subtype = 'PCM_16'
+                    is_write = True
+
+                if samplerate > 44100:
+                    samplerate = 44100
+                    is_write = True
+
+                if is_write:
+                    sf.write(sound_file.name, data, samplerate, subtype)
             finally:
                 sound_file.close()
         bar.next()
@@ -30,4 +43,3 @@ if error_list:
             f.write("%s\n" % item)
             print(item)
         f.close()
-
