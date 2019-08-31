9148a75a518c49f9a36ffb635a89997778f6a5c8
Gábor <gabor.gyorgy@penceo.com>
2019-08-31 22:40:28 +0200

    iterate through a folder and check the bitrate of wavs. Progressbar added

--- convert-audion.py
+++ convert-audion.py
@@ -1,9 +1,21 @@
 import os
 from glob import iglob
+import numpy as np
+import soundfile as sf
+from progress.bar import ChargingBar
 from folder import folder
 
-rootdir_glob = folder
-file_list = [f for f in iglob('**/*', recursive=True) if os.path.isfile(f)]
+file_list = [f for f in iglob(folder, recursive=True) if os.path.isfile(f)]
 
-for f in file_list:
-    print(f)
+with ChargingBar('Processing', max=len(file_list)) as bar:
+    for f in file_list:
+        if f.lower().endswith('wav'):
+            try:
+                sound_file = sf.SoundFile(f)
+            except:
+                print('ERROR', f)
+            else:
+                if sound_file.subtype != 'PCM_16':
+                    print(sound_file.subtype)
+        bar.next()
+
